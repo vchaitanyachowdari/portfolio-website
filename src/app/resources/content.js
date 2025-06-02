6fab8c8a1af1098e4f488e33ffb7558bd1213f72
V Chaitanya Chowdari <vchaitanyachowdari@users.noreply.github.com>
2025-06-02 22:01:09 +0530

    Initial commit or update

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -1,16 +1,16 @@
 import { Logo } from "@/once-ui/components";
 
 const person = {
-  firstName: "Selene",
-  lastName: "Yu",
+  firstName: "V Chaitanya",
+  lastName: "Chowdari",
   get name() {
     return `${this.firstName} ${this.lastName}`;
   },
-  role: "Design Engineer",
+  role: "AI Generlist",
   avatar: "/images/avatar.jpg",
-  email: "example@gmail.com",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  email: "vchaitanya@chowdari.in",
+  location: "Asia/Kolkata", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["English", "Kannada", "Hindi", "Telugu" ], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter = {
@@ -30,17 +30,17 @@ const social = [
   {
     name: "GitHub",
     icon: "github",
-    link: "https://github.com/once-ui-system/nextjs-starter",
+    link: "https://github.com/vchaitanyachowdari",
   },
   {
     name: "LinkedIn",
     icon: "linkedin",
-    link: "https://www.linkedin.com/company/once-ui/",
+    link: "https://www.linkedin.com/in/vchaitanyachowdari",
   },
   {
     name: "X",
     icon: "x",
-    link: "",
+    link: "https://x.com/vchaitanyachai?s=11",
   },
   {
     name: "Email",
@@ -55,16 +55,16 @@ const home = {
   label: "Home",
   title: `${person.name}'s Portfolio`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
-  headline: <>Building bridges between design and code</>,
+  headline: <>Building bridges between Technology and Innovation</>,
   featured: {
     display: true,
-    title: <>Recent project: <strong className="ml-4">Once UI</strong></>,
-    href: "/work/building-once-ui-a-customizable-design-system",
+    title: <>Recent project: <strong className="ml-4">Portfolio Website</strong></>,
+    href: "https://chowdari.in",
   },
   subline: (
     <>
-      I'm Selene, a design engineer at <Logo icon={false} style={{ display: "inline-flex", top: "0.25em", marginLeft: "-0.25em" }}/>, where I craft intuitive
-      <br /> user experiences. After hours, I build my own projects.
+      I'm V Chaitanya Chowdari, a AI Generalist at VC AI Creator, where I craft intuitive
+      <br /> user needs. After hours, I build my own projects.
     </>
   ),
 };
@@ -83,7 +83,7 @@ const about = {
   },
   calendar: {
     display: true,
-    link: "https://cal.com",
+    link: "https://form.typeform.com/to/BaPLDCSt",
   },
   intro: {
     display: true,
@@ -101,33 +101,40 @@ const about = {
     title: "Work Experience",
     experiences: [
       {
-        company: "FLY",
-        timeframe: "2022 - Present",
-        role: "Senior Design Engineer",
+        company: "VC AI Creator",
+        timeframe: "2025 - Present",
+        role: "Founder and CEO",
         achievements: [
           <>
-            Redesigned the UI/UX for the FLY platform, resulting in a 20% increase in user
-            engagement and 30% faster load times.
+            Defined the vision, strategy, and roadmap for Vc AI Creator driving innovation at the intersection of AI, automation, and product development.
           </>,
           <>
-            Spearheaded the integration of AI tools into design workflows, enabling designers to
-            iterate 50% faster.
+            Built and managed cross-functional teams, collaborating with designers, engineers, and product managers to deliver high impact solutions.
+          </>,
+          <>
+            Represented the company in partnerships, pitches, and technical discussions, aligning business objectives with product execution.
+          </>,
+          <>
+            Drove go-to-market strategies, positioning Vc AI Creator as a forward-thinking brand in AI automation, voice agents, and Web3.
+          </>,
+          <>
+            Led architecture and systems design, implementing scalable, modular platforms using React.js, React Native, and AI integration pipelines.
           </>,
         ],
         images: [
           // optional: leave the array empty if you don't want to display images
           {
-            src: "/images/projects/project-01/cover-01.jpg",
-            alt: "Once UI Project",
+            src: "/images/company/vcaicreator/vcaicreator-01.png",
+            alt: "Pitch Disc Slides",
             width: 16,
             height: 9,
           },
         ],
       },
       {
-        company: "Creativ3",
-        timeframe: "2018 - 2022",
-        role: "Lead Designer",
+        company: "EventureX",
+        timeframe: "2025 - Present",
+        role: "Founder",
         achievements: [
           <>
             Developed a design system that unified the brand across multiple platforms, improving
@@ -140,6 +147,23 @@ const about = {
         ],
         images: [],
       },
+      {
+        company: "Physics Mindboggler",
+        timeframe: "Jan 2025 - Apr 2025",
+        role: "Business Development Executive",
+        achievements: [
+          <>
+            Spearheaded the development and maintenance of responsive web and mobile applications utilizing React.js and React Native, ensuring seamless user experiences across platforms.
+          </>,
+          <>
+            Collaborated cross-functionally with designers, product managers, and fellow developers to deliver high-quality products that meet client needs and market demands.
+          </>,
+          <>
+            Implemented responsive design principles and ensured cross-browser compatibility to enhance accessibility and user engagement.
+          </>,
+        ],
+        images: [],
+      },
     ],
   },
   studies: {
@@ -147,50 +171,170 @@ const about = {
     title: "Studies",
     institutions: [
       {
-        name: "University of Jakarta",
-        description: <>Studied software engineering.</>,
+          name: "100x Engineering",
+          description: <>Learing and Implimenting Generative AI</>,
       },
       {
-        name: "Build the Future",
-        description: <>Studied online marketing and personal branding.</>,
+        name: "Indian Institute of Technology, Indore",
+        description: <>Professional Certificate Program in Artificial Intelligence and Data Science</>,
+      },
+      {
+        name: "Babuji Institute of Engineering and Technology",
+        description: <>Studing Computer Science and Engineering</>,
       },
     ],
   },
   technical: {
     display: true, // set to false to hide this section
-    title: "Technical skills",
+    title: "Technical Skills",
     skills: [
+       {
+        title: "C++",
+        description: <>Proficient in writing optimized, object-oriented C++ code for system-level programming, algorithms, and competitive coding.</>,
+        // optional: leave the array empty if you don't want to display images
+        images: [ ],
+      },
       {
-        title: "Figma",
-        description: <>Able to prototype in Figma with Once UI with unnatural speed.</>,
+        title: "Python",
+        description: <>Skilled in using Python for data analysis, machine learning, automation, and building AI-powered applications.</>,
+        // optional: leave the array empty if you don't want to display images
+        images: [ ],
+      },
+      {
+        title: "HTML 5",
+        description: <>Experienced in crafting semantic, responsive HTML5 layouts for modern web applications with accessibility and SEO in mind.</>,
+        // optional: leave the array empty if you don't want to display images
+        images: [ ],
+      },
+      {
+        title: "CSS",
+        description: <>Expertise in styling UI using CSS3, Flexbox, Grid, and animations to create sleek, responsive designs across devices.</>,
+        // optional: leave the array empty if you don't want to display images
+        images: [ ],
+      },
+      {
+        title: "Java",
+        description: <>Familiar with Java for building robust backend systems, OOP design, and Android app development.</>,
+        // optional: leave the array empty if you don't want to display images
+        images: [ ],
+      },
+      {
+        title: "My SQL",
+        description: <>Skilled in designing and managing relational databases using MySQL, including complex queries, joins, and schema optimization.</>,
+        // optional: leave the array empty if you don't want to display images
+        images: [ ],
+      },
+      {
+        title: "Git",
+        description: <>Experienced in using Git for version control, collaborative development, and maintaining clean code history.</>,
+        // optional: leave the array empty if you don't want to display images
+        images: [ ],
+      },
+      {
+        title: "GitHub",
+        description: <>Proficient in leveraging GitHub for code hosting, project management, and CI/CD workflows.</>,
+        // optional: leave the array empty if you don't want to display images
+        images: [ ],
+      },
+      {
+        title: "Prompt Engineerng",
+        description: <>Skilled in designing, refining, and optimizing prompts for LLMs (e.g., GPT, Gemini) to deliver accurate, context-aware responses and automate tasks.</>,
         // optional: leave the array empty if you don't want to display images
         images: [
-          {
-            src: "/images/projects/project-01/cover-02.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-          {
-            src: "/images/projects/project-01/cover-03.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
+            {
+                src: "/images/skills/promptengineering/promptformula.png",
+                alt: "Prompt Engineering Formula",
+                width: 16,
+                height: 9,
           },
         ],
       },
       {
-        title: "Next.js",
-        description: <>Building next gen apps with Next.js + Once UI + Supabase.</>,
+        title: "Make.com",
+        description: <>Proficient in using Make.com for building no-code/low-code automation workflows across apps.</>,
+        // optional: leave the array empty if you don't want to display images
+          images: [
+                   {
+                       src: "/images/skills/make/make-01.png",
+                       alt: "Sync Notion Databases to Google Calendar",
+                       width: 16,
+                       height: 9,
+                   },
+                   {
+                       src: "/images/skills/make/make-02.png",
+                       alt: "Email Automation",
+                       width: 16,
+                       height: 9,
+                   },
+                 ],
+      },
+      {
+        title: "n8n",
+        description: <>Experienced in building end-to-end AI and automation pipelines using n8n, integrating APIs, webhooks, agents, and advanced logic flows.</>,
         // optional: leave the array empty if you don't want to display images
         images: [
-          {
-            src: "/images/projects/project-01/cover-04.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-        ],
+                 {
+                     src: "/images/skills/n8n/n8n-01.png",
+                     alt: "Deap Research Agent",
+                     width: 16,
+                     height: 9,
+                 },
+                 {
+                     src: "/images/skills/n8n/n8n-02.png",
+                     alt: "SEO Audit Report Generater",
+                     width: 16,
+                     height: 9,
+                 },
+                 {
+                     src: "/images/skills/n8n/n8n-03.png",
+                     alt: "Social Media Content Automation",
+                     width: 16,
+                     height: 9,
+                 },
+                 {
+                     src: "/images/skills/n8n/n8n-04.png",
+                     alt: "Email Summering Agent",
+                     width: 16,
+                     height: 9,
+                 },
+               ],
+      },
+      {
+        title: "Zapier",
+        description: <>Able to automate multi-step workflows using Zapier, integrating tools like Google Sheets, Gmail, Notion, and CRMs for lead gen and email tasks.</>,
+        // optional: leave the array empty if you don't want to display images
+          images: [
+                   {
+                       src: "/images/skills/zapier/zapier-01.png",
+                       alt: "Email Automation",
+                       width: 16,
+                       height: 9,
+                   },
+                   {
+                       src: "/images/skills/zapier/zapier-01.png",
+                       alt: "Lead Generation",
+                       width: 16,
+                       height: 9,
+                   },
+                 ],
+       },
+      {
+        title: "AI Agents",
+        description: <>Skilled in designing and deploying AI agents for research, customer support, data extraction, and intelligent automation using LLMs and tools like LangChain.</>,
+        // optional: leave the array empty if you don't want to display images
+        images: [ ],
+      },
+      {
+        title: "v0",
+        description: <>Proficient in using Vercel’s v0 to rapidly prototype frontend UIs using AI-powered generation and Tailwind CSS integration.</>,
+        // optional: leave the array empty if you don't want to display images
+        images: [ ],
+      },
+      {
+        title: "Lovable",
+        description: <>Experience using Lovable for AI-powered content generation, idea validation, and business exploration tools..</>,
+        // optional: leave the array empty if you don't want to display images
+        images: [ ],
       },
     ],
   },
@@ -219,46 +363,85 @@ const gallery = {
   label: "Gallery",
   title: `Photo gallery – ${person.name}`,
   description: `A photo collection by ${person.name}`,
-  // Images by https://lorant.one
   // These are placeholder images, replace with your own
   images: [
     {
-      src: "/images/gallery/horizontal-1.jpg",
+      src: "/images/gallery/horizontal-01.jpg",
       alt: "image",
       orientation: "horizontal",
     },
     {
-      src: "/images/gallery/horizontal-2.jpg",
+      src: "/images/gallery/horizontal-02.jpg",
       alt: "image",
       orientation: "horizontal",
     },
     {
-      src: "/images/gallery/horizontal-3.jpg",
+      src: "/images/gallery/horizontal-03.jpg",
       alt: "image",
       orientation: "horizontal",
     },
     {
-      src: "/images/gallery/horizontal-4.jpg",
+      src: "/images/gallery/horizontal-04.jpg",
       alt: "image",
       orientation: "horizontal",
     },
     {
-      src: "/images/gallery/vertical-1.jpg",
+      src: "/images/gallery/horizontal-05.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/horizontal-06.jpeg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/horizontal-07.jpeg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/horizontal-08.png",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/horizontal-09.jpeg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/horizontal-10.jpeg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/horizontal-11.jpeg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/vertical-01.png",
       alt: "image",
       orientation: "vertical",
     },
     {
-      src: "/images/gallery/vertical-2.jpg",
+      src: "/images/gallery/vertical-02.jpeg",
       alt: "image",
       orientation: "vertical",
     },
     {
-      src: "/images/gallery/vertical-3.jpg",
+      src: "/images/gallery/vertical-03.jpeg",
       alt: "image",
       orientation: "vertical",
     },
     {
-      src: "/images/gallery/vertical-4.jpg",
+      src: "/images/gallery/vertical-04.jpeg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/vertical-05.jpeg",
       alt: "image",
       orientation: "vertical",
     },
