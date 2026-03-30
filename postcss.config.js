e3b2fec8078e4872ab4f8288a5ed5108b3e8c86b
V Chaitanya Chowdari <vchaitanyachowdari@users.noreply.github.com>
2026-03-30 10:20:42 +0530

    fix: Revert PostCSS plugin configuration to string format for Next.js compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
+    "tailwindcss",
+    "autoprefixer",
     [
       "@csstools/postcss-global-data",
       {
