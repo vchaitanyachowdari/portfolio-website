752e6cabfa7d05980277d468e095d459425da808
V Chaitanya Chowdari <vchaitanyachowdari@users.noreply.github.com>
2026-03-30 10:18:50 +0530

    fix: Correct PostCSS configuration for Tailwind CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
-    "tailwindcss",
-    "autoprefixer",
+    require('tailwindcss'),
+    require('autoprefixer'),
     [
       "@csstools/postcss-global-data",
       {
