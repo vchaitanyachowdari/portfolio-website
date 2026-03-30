2890b0ad65448f8c42f99337e7f673a271053878
V Chaitanya Chowdari <vchaitanyachowdari@users.noreply.github.com>
2026-03-30 10:36:04 +0530

    refactor: Revert postcss.config.js and update package files after build attempts

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,7 @@
+// postcss.config.js
 module.exports = {
-  plugins: [
-    "tailwindcss",
-    "autoprefixer",
-    [
-      "@csstools/postcss-global-data",
-      {
-        files: ["src/once-ui/styles/breakpoints.scss"],
-      },
-    ],
-    "postcss-custom-media",
-    "postcss-flexbugs-fixes",
-    [
-      "postcss-preset-env",
-      {
-        autoprefixer: {
-          flexbox: "no-2009",
-        },
-        stage: 3,
-        features: {
-          "custom-properties": false,
-        },
-      },
-    ],
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
